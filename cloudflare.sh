37087d07c18a6927191f9eb2271a528d2e552d8b
TommyTran732 <57488583+tommytran732@users.noreply.github.com>
2020-01-02 17:21:51 -0500

    Update cloudflare.sh

--- cloudflare.sh
+++ cloudflare.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+output(){
+    echo -e '\e[36m'$1'\e[0m';
+}
+
 if [ "$lsb_dist" =  "ubuntu" ] || [ "$lsb_dist" =  "debian" ]; then
      apt -y install ufw
 elif [ "$lsb_dist" =  "fedora" ] || [ "$lsb_dist" =  "rhel" ] || [ "$lsb_dist" =  "centos" ]; then

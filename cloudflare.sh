27f27d6218b0e598d56921f92a4ff3633eefe841
TommyTran732 <57488583+tommytran732@users.noreply.github.com>
2020-01-02 17:44:36 -0500

    Update cloudflare.sh

--- cloudflare.sh
+++ cloudflare.sh
@@ -9,54 +9,19 @@ if [ -r /etc/os-release ]; then
 fi
 
 if [ "$lsb_dist" =  "ubuntu" ] || [ "$lsb_dist" =  "debian" ]; then
-     apt -y install ufw
+     apt -y install ufw wget
      # Opening Port 22 just in case so that we do not lose the internet connection when the rules are applied.
      ufw allow 22
-     ufw allow from 173.245.48.0/20 to any proto tcp port 80
-     ufw allow from 103.21.244.0/22 to any proto tcp port 80
-     ufw allow from 103.22.200.0/22 to any proto tcp port 80
-     ufw allow from 103.31.4.0/22 to any proto tcp port 80
-     ufw allow from 141.101.64.0/18 to any proto tcp port 80
-     ufw allow from 108.162.192.0/18 to any proto tcp port 80
-     ufw allow from 190.93.240.0/20 to any proto tcp port 80
-     ufw allow from 188.114.96.0/20 to any proto tcp port 80
-     ufw allow from 197.234.240.0/22 to any proto tcp port 80
-     ufw allow from 198.41.128.0/17 to any proto tcp port 80
-     ufw allow from 162.158.0.0/15 to any proto tcp port 80
-     ufw allow from 104.16.0.0/12 to any proto tcp port 80
-     ufw allow from 172.64.0.0/13 to any proto tcp port 80
-     ufw allow from 131.0.72.0/22 to any proto tcp port 80
-     ufw allow from 2400:cb00::/32 to any proto tcp port 80
-     ufw allow from 2606:4700::/32 to any proto tcp port 80
-     ufw allow from 2803:f800::/32 to any proto tcp port 80
-     ufw allow from 2405:b500::/32 to any proto tcp port 80
-     ufw allow from 2405:8100::/32 to any proto tcp port 80
-     ufw allow from 2a06:98c0::/29 to any proto tcp port 80
-     ufw allow from 2c0f:f248::/32 to any proto tcp port 80
-     ufw allow from 173.245.48.0/20 to any proto tcp port 443
-     ufw allow from 103.21.244.0/22 to any proto tcp port 443
-     ufw allow from 103.22.200.0/22 to any proto tcp port 443
-     ufw allow from 103.31.4.0/22 to any proto tcp port 443
-     ufw allow from 141.101.64.0/18 to any proto tcp port 443
-     ufw allow from 108.162.192.0/18 to any proto tcp port 443
-     ufw allow from 190.93.240.0/20 to any proto tcp port 443
-     ufw allow from 188.114.96.0/20 to any proto tcp port 443
-     ufw allow from 197.234.240.0/22 to any proto tcp port 443
-     ufw allow from 198.41.128.0/17 to any proto tcp port 443
-     ufw allow from 162.158.0.0/15 to any proto tcp port 443
-     ufw allow from 104.16.0.0/12 to any proto tcp port 443
-     ufw allow from 172.64.0.0/13 to any proto tcp port 443
-     ufw allow from 131.0.72.0/22 to any proto tcp port 443
-     ufw allow from 2400:cb00::/32 to any proto tcp port 443
-     ufw allow from 2606:4700::/32 to any proto tcp port 443
-     ufw allow from 2803:f800::/32 to any proto tcp port 443
-     ufw allow from 2405:b500::/32 to any proto tcp port 443
-     ufw allow from 2405:8100::/32 to any proto tcp port 443
-     ufw allow from 2a06:98c0::/29 to any proto tcp port 443
-     ufw allow from 2c0f:f248::/32 to any proto tcp port 443
+     wget https://www.cloudflare.com/ips-v4
+     wget https://www.cloudflare.com/ips-v6
+     
      yes | ufw enable
 elif [ "$lsb_dist" =  "fedora" ] || [ "$lsb_dist" =  "rhel" ] || [ "$lsb_dist" =  "centos" ]; then
-     yum -y install firewalld
+     yum -y install firewalld wget
+     wget https://www.cloudflare.com/ips-v4
+     wget https://www.cloudflare.com/ips-v6
+     
+     firewall-cmd --reload
 else 
      output "Unsupported distribution. This script only supports Fedora, RHEL, CentOS, Ubuntu, and Debian."
 fi 

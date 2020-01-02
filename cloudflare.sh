c4fda65a7586fe022c8a0d31c9dcb79d334c2d40
TommyTran732 <57488583+tommytran732@users.noreply.github.com>
2020-01-02 17:21:21 -0500

    Update cloudflare.sh

--- cloudflare.sh
+++ cloudflare.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+if [ "$lsb_dist" =  "ubuntu" ] || [ "$lsb_dist" =  "debian" ]; then
+     apt -y install ufw
+elif [ "$lsb_dist" =  "fedora" ] || [ "$lsb_dist" =  "rhel" ] || [ "$lsb_dist" =  "centos" ]; then
+     yum -y install firewalld
+else 
+     output "Unsupported distribution. This script only supports Fedora, RHEL, CentOS, Ubuntu, and Debian."
+fi 
